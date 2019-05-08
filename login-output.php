c441746c7a26d9675782f1b02330502871c3d131
dmyr0613 <dmyr0613@gmail.com>
2019-05-08 14:40:16 +0900

    1

--- login-output.php
+++ login-output.php
@@ -14,11 +14,14 @@
 					unset($_SESSION['kanja']);
 					// $pdo=new PDO('mysql:host=localhost;dbname=marcs;charset=utf8', 'sbs', 'sbs_toro');
 
-					// $pdo=new PDO('mysql:host=10.165.224.2;dbname=d13p6kmhdcirvm;charset=utf8', 'gkijtxlavebgol', 'ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505');
+					// $dsn='postgres://gkijtxlavebgol:ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d13p6kmhdcirvm';
+					// $pdo = new PDO($dsn);
 
-					$dsn='postgres://gkijtxlavebgol:ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505@ec2-174-129-208-118.compute-1.amazonaws.com:5432/d13p6kmhdcirvm';
-					$pdo = new PDO($dsn);
-
+					$dsn = 'pgsql:dbname=d13p6kmhdcirvm host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432';
+					$user = 'gkijtxlavebgol';
+					$password = 'ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505';
+					$pdo = new PDO($dsn, $user, $password);
+							
 					$sql=$pdo->prepare('select * from kanja where kanja_id=? and password=?');
 					$sql->execute([$_REQUEST['kanja_id'], $_REQUEST['password']]);
 					foreach ($sql as $row) {
