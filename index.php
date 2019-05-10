f24f6579cd7f6afc91c304109ed4ff8a3dcea24a
dmyr0613 <dmyr0613@gmail.com>
2019-05-10 16:49:42 +0900

    1

--- index.php
+++ index.php
@@ -87,8 +87,9 @@ foreach ($events as $event) {
     $obj = json_decode($jsonString, true);
 
     print_r($obj);
+    $messageStr = '現在の診察状況' . $obj;
 
-    $messageStr = '現在の診察状況';
+    // $messageStr = '現在の診察状況';
     foreach ($obj as $key => $val){
       error_log($key);
       $messageStr = $messageStr . "\r\n";
