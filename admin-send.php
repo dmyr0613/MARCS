dfc11b851cf486642e3d7fc9de9d5082830bad99
dmyr0613 <dmyr0613@gmail.com>
2019-06-06 17:45:46 +0900

    1

--- admin-send.php
+++ admin-send.php
@@ -88,7 +88,8 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 								$url = "https://sand-api-smslink.nexlink2.jp/api/v1/delivery";
 
 								// $json = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": " こんにちは", "reserved_at": "",　"click_count": true,　"notification_emails": [　"test1@example.com（通知先１）",　"test2@example.com（通知先２）"]}';
-								$json = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": " こんにちは", "reserved_at":"", "click_count": true, "notification_emails": [ "test1@example.com（通知先１）", "test2@example.com（通知先２）"]}';
+								// $json = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": " こんにちは", "reserved_at":"", "click_count": true, "notification_emails": [ "test1@example.com（通知先１）", "test2@example.com（通知先２）"]}';
+								$json = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": " こんにちは" }';
 								$content = json_decode($json);
 
 								$options = array('http' => array(
