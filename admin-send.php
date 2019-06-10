3dcc45124bba7b504aa3fa8efbf47e19523b6ad1
dmyr0613 <dmyr0613@gmail.com>
2019-06-10 18:44:55 +0900

    1

--- admin-send.php
+++ admin-send.php
@@ -127,8 +127,8 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 
 								$ch = curl_init();
 								curl_setopt($ch, CURLOPT_URL, 'https://sand-api-smslink.nexlink2.jp/api/v1/delivery');
-								// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json'));
-								curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json'));
+								curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json'));
+								// curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json'));
 								curl_setopt($ch, CURLOPT_POST, 1);
 								curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options));
 								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
