00c24301cbdb91872c052b62bdd602c61011b0d3
dmyr0613 <dmyr0613@gmail.com>
2019-06-10 19:05:05 +0900

    1

--- admin-send.php
+++ admin-send.php
@@ -120,11 +120,11 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 
 								$ch = curl_init();
 								curl_setopt($ch, CURLOPT_URL, 'https://sand-api-smslink.nexlink2.jp/api/v1/delivery');
-								curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json'));
+								curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: dbe1aee9-93e5-4d28-b445-f166dea93658', 'Content-Type: application/json', 'Host: sand-api-smslink.nexlink2.jp'));
 								curl_setopt($ch, CURLOPT_POST, 1);
 								curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options));
 								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-								
+
 								$response = curl_exec($ch);
 								curl_close($ch);
 								error_log(print_r($response, true));
