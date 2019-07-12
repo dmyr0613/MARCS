4932281662d7e706797c76c5dfd82ffb1f3ec6fc
dmyr0613 <dmyr0613@gmail.com>
2019-07-12 13:12:27 +0900

    1

--- js.js
+++
@@ -1,7 +0,0 @@
-function test(val) {
-    // テスト用に id タイトルを変更
-    var h1 = document.getElementById("title");
-    h1.textContent = val;
-
-    alart(val);
-}
--- pktouch.php
+++ pktouch.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <title>ページタイトル</title>
-    </head>
-    <body>
-        <h1 id="title">タイトル</h1>
-
-        <script type="text/javascript" src="js.js"></script>
-
-        <FORM method="POST">
-        <P><INPUT type="button" name="B1" value="TEST" onclick="test()"></P>
-        </FORM>
-    </body>
-</html>
+<HTML>
+<HEAD><TITLE>テスト</TITLE></HEAD>
+<BODY>
+<SCRIPT language="JavaScript">
+<!--
+  function msgdsp() {
+      alert("メッセージ");
+  }
+// -->
+</SCRIPT>
+<FORM method="POST"><P>
+<INPUT type="button" name="B1"
+value="TEST"
+onclick="msgdsp()"></P>
+</FORM>
+</BODY>
+</HTML>
