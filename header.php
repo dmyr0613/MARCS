345cbd15f0f1f1274f7b54b9205a3746f0d9f428
dmyr0613 <dmyr0613@gmail.com>
2022-04-04 15:26:33 +0900

    1

--- header.php
+++ header.php
@@ -16,9 +16,13 @@
   // $pdo=new PDO('mysql:host=localhost;dbname=marcs;charset=utf8', 'sbs', 'sbs_toro');
 
   //Heroku PostgresSQL
-  $dsn = 'pgsql:dbname=d13p6kmhdcirvm host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432';
-  $user = 'gkijtxlavebgol';
-  $password = 'ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505';
+  // $dsn = 'pgsql:dbname=d13p6kmhdcirvm host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432';
+  // $user = 'gkijtxlavebgol';
+  // $password = 'ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505';
+
+  $dsn = 'pgsql:dbname=dbejrl8163lahl host=ec2-54-156-80-253.compute-1.amazonaws.com port=5432';
+  $user = 'ocmkfhxxlrdqcl';
+  $password = '19a9523daf9e628a66d12d0a17aa5a0f9505d09ede0aa9857dccedc78c4f4a1f';
   $pdo = new PDO($dsn, $user, $password);
 ?>
 
